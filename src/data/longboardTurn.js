b7d70309501a237d33d38c59b1b40f9c8fbd13d6
Healy <xianglitm@163.com>
2023-09-15 17:21:53 +0800

    optimize

---
+++ src/data/longboardTurn.js
@@ -0,0 +1,26 @@
+const longboardTurn = {
+  id: "longboardTurn",
+  cover: {},
+  content: [
+    {
+      bigTitleText: "长板冲浪转向技巧",
+      tipTitleText: "概述",
+      contentText:
+        "转向是冲浪中基本且重要的技巧，只有掌握了良好的转向才能为做其他动作打下基础。\n长板由于板子很长和宽，所以转向时需要更多技巧。",
+    },
+    {
+      bigTitleText: "长板冲浪转向技巧",
+      tipTitleText: "板倾斜",
+      contentText:
+        "倾斜是指，沿板子宽度方向左右的倾斜。\n在转向的时候，要让脚踩在板的中心线偏左或者偏右，这样才能让板倾斜，从而利于转向。\n",
+    },
+    {
+      bigTitleText: "长板冲浪转向技巧",
+      tipTitleText: "板尾下压",
+      contentText:
+        "倾斜是指，板子宽度方向左右的倾斜。\n在转向的时候，要让脚踩在板的中心线偏左或者偏右，这样才能让板倾斜，从而利于转向。\n",
+    },
+  ],
+};
+
+module.exports = { longboardTurn };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ const fs = require("fs");
 const path = require("path");
 
 const CardRender = require("./text2image");
+const { longboardTurn } = require("./data/longboardTurn");
 
-const Card1 = {
-  bigTitleText: "长板冲浪转向技巧",
-  tipTitleText: "板倾斜",
-  contentText:
-    "倾斜是指，板子宽度方向左右的倾斜。\n在转向的时候，要让脚踩在板的中心线偏左或者偏右，这样才能让板倾斜，从而利于转向。\n",
-  targetPath: path.join(__dirname, "./resImages/turnCard1.png"),
-};
+function checkDirExist(dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+}
 
-CardRender(Card1);
+const contentId = longboardTurn.id;
+const targetDir = path.join(__dirname, `./resImages/${contentId}`);
+
+checkDirExist(targetDir);
+
+longboardTurn.content.forEach((card, index) => {
+  CardRender({
+    ...card,
+    targetPath: path.join(targetDir, `./contet_${index}.png`),
+  });
+});
